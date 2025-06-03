929f798404932205b77e5dd7bbfe8bfc84c13797
Konstantin <ikv1980@gmail.com>
2025-06-03 09:32:48 +0300

    Поправил конверторы

--- Project/Tools/Converters/PriceConverter.cs
+++ Project/Tools/Converters/PriceConverter.cs
@@ -7,17 +7,27 @@ namespace Project.Tools
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var russianCulture = new CultureInfo("ru-RU");
+            
             if (value is decimal price)
             {
-                return price.ToString("N2", culture) + " ₽";
+                return price.ToString("N2", russianCulture) + " ₽";
             }
 
-            return "0 ₽";
+            return "0,00 ₽";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value;
+            if (value is string stringValue)
+            {
+                if (decimal.TryParse(stringValue.Replace(" ₽", ""), NumberStyles.Any, new CultureInfo("ru-RU"), out decimal price))
+                {
+                    return price;
+                }
+            }
+            
+            return 0m;
         }
     }
 }
--- Project/Tools/Converters/TotalPriceConverter.cs
+++ Project/Tools/Converters/TotalPriceConverter.cs
@@ -11,6 +11,7 @@ namespace Project.Tools
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var russianCulture = new CultureInfo("ru-RU");
             if (value is ulong orderId)
             {
                 using (var db = new Db())
@@ -19,16 +20,24 @@ namespace Project.Tools
                         .Where(oc => oc.OrderId == orderId)
                         .Sum(oc => oc.Car.Price);
 
-                    return totalPrice.ToString("N0", culture) + " ₽";
+                    return totalPrice.ToString("N0", russianCulture) + " ₽";
                 }
             }
 
-            return "0 ₽";
+            return "0,00 ₽";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value;
+            if (value is string stringValue)
+            {
+                if (decimal.TryParse(stringValue.Replace(" ₽", ""), NumberStyles.Any, new CultureInfo("ru-RU"), out decimal price))
+                {
+                    return price;
+                }
+            }
+            
+            return 0m;
         }
     }
 }

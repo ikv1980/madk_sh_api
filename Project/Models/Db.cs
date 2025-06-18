61d9495ed8631daf99c0d0c8b223e2b74aea5323
Konstantin <ikv1980@gmail.com>
2025-06-18 23:44:52 +0300

    Новый сервер

--- Project/Models/Db.cs
+++ Project/Models/Db.cs
@@ -77,7 +77,7 @@ public partial class Db : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=213.171.25.72;port=3306;database=madk;uid=madk;pwd=Kostik80", Microsoft.EntityFrameworkCore.ServerVersion.Parse("11.5.2-mariadb"));
+        => optionsBuilder.UseMySql("server=82.202.140.44;port=5000;database=madk;uid=admin;pwd=Kostik8o", Microsoft.EntityFrameworkCore.ServerVersion.Parse("11.5.2-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
